99ae151263c25b0834a4e20e155652964cffc0f6
yuqi141421 <307530884@qq.com>
2019-04-05 21:39:50 +0800

    3 spring cloud config

--- 2-1-config-server/src/test/java/com/jk/learn/configserver/ConfigServerApplicationTests.java
+++ 2-1-config-server/src/test/java/com/jk/learn/configserver/ConfigServerApplicationTests.java
@@ -5,12 +5,27 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Enumeration;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ConfigServerApplicationTests {
 
     @Test
     public void contextLoads() {
+        try {
+            Enumeration<URL> enumeration = getClass().getClassLoader().getResources("META-INF/spring.factories");
+            while (enumeration.hasMoreElements()) {
+                System.out.println(enumeration.nextElement().toString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
